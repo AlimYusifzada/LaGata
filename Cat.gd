79e6186195e94312e5c33483f87e9f8041c2f20b
Alim Yusif-zada <yusifzaj@gmail.com>
2022-10-28 22:12:08 +0400

    cat dynamics
    jump off the platform

--- Cat.gd
+++ Cat.gd
@@ -6,7 +6,9 @@ const GRAVITY=1400
 const JUMP_VELOCITY=-700
 const INERTIA=SPEED
 const UP=Vector2(0,-1)
-
+const PLATFORM=1            # platforms layer, user can jump on and off the platform
+const GROUND=2 				# ground layer, user cant jump off the ground 
+							# layer 0 is a player 
 var velocity=Vector2()
 var Life=100
 var Power=100
@@ -19,25 +21,29 @@ func _ready():
 func _physics_process(delta):
 	fall(delta)
 	jump()
+	jumpdown() # jump of the platform (while press down togle mask bit 1 for 0.5 sec)
 	walk(delta)
 	move_and_slide(velocity,UP)
-	pass
 
-func _process(delta):
+func _process(delta): 
 	animate()
 	
 func fall(delta):
 	if is_on_floor():
 		velocity.y=0
-	elif is_on_ceiling():
-		velocity.y=1
 	else:
 		velocity.y+=GRAVITY*delta
 		
-		
+func jumpdown():
+	if Input.is_action_pressed("ui_down") and is_on_floor():
+		set_collision_mask_bit(PLATFORM,false)
+		$jumptimer.start()
+			
 func jump():
 	if Input.is_action_pressed("ui_up") and is_on_floor():
 		velocity.y=JUMP_VELOCITY
+		set_collision_mask_bit(PLATFORM,false)
+		$jumptimer.start()		
 			
 func walk(delta):
 	if Input.is_action_pressed("ui_right"): # and not Input.is_action_pressed("ui_left"):
@@ -65,3 +71,6 @@ func animate():
 		$AnimatedSprite.play("Jump")
 			
 
+func _on_jumptimer_timeout():
+	set_collision_mask_bit(PLATFORM,true)
+	
