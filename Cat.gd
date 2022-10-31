4578c688590aa1f52dd807e619192a5a056ce18d
Alim Yusif-zada <yusifzaj@gmail.com>
2022-10-31 23:19:03 +0400

    move dynamics
    move dynamics

--- Cat.gd
+++ Cat.gd
@@ -1,8 +1,8 @@
 extends KinematicBody2D
 
 
-const SPEED=250   #walk speed
-const RSPEED=500  #run speed
+const SPEED=250   #walking speed
+const MAXSPEED=500
 const GRAVITY=1400
 const JUMP_VELOCITY=-700
 const UP=Vector2(0,-1)
@@ -17,7 +17,7 @@ var Power=100
 
 func _ready():
 	randomize()
-	$AnimatedSprite.playing=false
+	$AnimatedSprite.playing=true
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -26,9 +26,8 @@ func _physics_process(delta):
 	#---FASTER---
 	fall(delta)
 	jump()
-	jumpdown() # jump of the platform (while press down togle mask bit 1 for 0.5 sec)
 	run(delta)
-	walk(delta)
+	#walk(delta)
 	#---SLOWER---
 	move_and_slide(velocity,UP)
 
@@ -41,69 +40,56 @@ func fall(delta):
 	else:
 		velocity.y+=GRAVITY*delta
 		
-func jumpdown():
-	if Input.is_action_pressed("ui_down") and is_on_floor():
-		$jumptimer.start()
-		set_collision_mask_bit(PLATFORM,false)
-			
 func jump():
 	if Input.is_action_pressed("ui_up") and is_on_floor():
 		$jumptimer.start()		
 		velocity.y=JUMP_VELOCITY
 		set_collision_mask_bit(PLATFORM,false)
-
+	elif Input.is_action_pressed("ui_down") and is_on_floor():
+		$jumptimer.start()
+		set_collision_mask_bit(PLATFORM,false)
+			
 func run(delta):
 	if Input.is_action_pressed("ui_runright"):#and not Input.is_action_pressed("ui_runright"):
-		velocity.x=RSPEED
+		if velocity.x<MAXSPEED:
+			velocity.x+=MAXSPEED*delta
+		else:
+			velocity.x=MAXSPEED
 		$AnimatedSprite.flip_h=true
 	elif Input.is_action_pressed("ui_runleft"):#and not Input.is_action_pressed("ui_runright"):
-		velocity.x=-RSPEED
+		if velocity.x>-MAXSPEED:
+			velocity.x-=MAXSPEED*delta
+		else:
+			velocity.x=-MAXSPEED
 		$AnimatedSprite.flip_h=false
 	else:
 		if velocity.x>0:
-			velocity.x-=RSPEED*delta
+			velocity.x-=MAXSPEED*delta
 			if velocity.x<0:
 				velocity.x=0
 		if velocity.x<0:
-			velocity.x+=RSPEED*delta
+			velocity.x+=MAXSPEED*delta
 			if velocity.x>0:
 				velocity.x=0
 		
-			
-func walk(delta):
-	if Input.is_action_pressed("ui_walkright"):#and not Input.is_action_pressed("ui_runright"):
-		velocity.x=SPEED
-		$AnimatedSprite.flip_h=true
-	elif Input.is_action_pressed("ui_walkleft"):#and not Input.is_action_pressed("ui_runright"):
-		velocity.x=-SPEED
-		$AnimatedSprite.flip_h=false
-	else:
-		if velocity.x>0:
-			velocity.x-=SPEED*delta
-			if velocity.x<0:
-				velocity.x=0
-		if velocity.x<0:
-			velocity.x+=SPEED*delta
-			if velocity.x>0:
-				velocity.x=0
 		
 func animate():
 	var taleswing=randf()/2
-	var vx=abs(velocity.x)
-	if vx<=SPEED and is_on_floor():
-		$AnimatedSprite.speed_scale=2
-		$AnimatedSprite.play("Walk")
-	if vx>SPEED and is_on_floor():
-		$AnimatedSprite.speed_scale=2
-		$AnimatedSprite.play("Run")
-	if vx==0 and is_on_floor():
-		$AnimatedSprite.speed_scale=taleswing
-		$AnimatedSprite.play("Sit")
+	#var zoom=abs(velocity.x)*0.07/MAXSPEED+1
+	#$Cam.set_zoom(Vector2(zoom,zoom))
 	if velocity.y!=0 and !is_on_floor():
 		$AnimatedSprite.speed_scale=5
 		$AnimatedSprite.play("Jump")
-			
-
+	if velocity.x==0 and is_on_floor():
+		$AnimatedSprite.speed_scale=taleswing
+		$AnimatedSprite.play("Sit")
+	elif abs(velocity.x)<=SPEED and is_on_floor():
+		$AnimatedSprite.speed_scale=2
+		$AnimatedSprite.play("Walk")
+	elif abs(velocity.x)>SPEED and is_on_floor():
+		$AnimatedSprite.speed_scale=2
+		$AnimatedSprite.play("Run")
+		
 func _on_jumptimer_timeout():
 	set_collision_mask_bit(PLATFORM,true)
 	
