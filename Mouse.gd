ae03edff13d80ec79c03005367687e2de833ef18
Alim <yusifzaj@gmail.com>
2022-11-06 22:04:54 +0400

    enemy or pray movement

--- Mouse.gd
+++ Mouse.gd
@@ -17,9 +17,33 @@ func _ready():
 	pass # Replace with function body.
 	
 func _physics_process(delta):
+	fall(delta)
+	move(delta)
 	move_and_slide(velocity,UP)
 	pass
+
+func _process(delta):
+	animation()
+	
 
 func _on_Area2D_body_entered(body):
 	print(body)
 	pass # Replace with function body.
+	
+func fall(delta):
+	if is_on_floor():
+		velocity.y=0
+	else:
+		velocity.y+=GRAVITY*delta
+
+func animation():
+	if velocity.x>0:
+		$AnimatedSprite.flip_h=false
+	else:
+		$AnimatedSprite.flip_h=true
+		
+func move(delta):
+	if is_on_floor() and is_on_wall():
+		velocity.x*=-1
+		
+	pass
