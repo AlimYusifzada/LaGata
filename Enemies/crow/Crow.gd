37eb678f0ebae672564d0af6a705ff9d030cc2f3
Alim Yusif-zada <yusifzaj@gmail.com>
2022-12-06 21:18:29 +0400

    proj structure change
    proj structure change

---
+++ Enemies/crow/Crow.gd
@@ -0,0 +1,30 @@
+extends Node2D
+
+var SHIT=preload("res://Enemies/crow/shit.tscn")
+var pooping=false
+var poopspawn=Vector2()
+
+func _ready():
+	Global.Crow=self
+	poopspawn.y=position.y
+	scale=Vector2(0.5,0.5)
+	pass
+
+func _on_Area2D_body_entered(body):
+	if body.is_in_group("Cats"):
+		body.emit_signal("Enemy")
+	pass # Replace with function body.
+
+func _on_shitzone_body_entered(body):
+	if body.is_in_group("Cats") && !pooping:
+		pooping=true
+		poopspawn.x=body.position.x
+	pass # Replace with function body.
+
+func _on_AnimatedSprite_animation_finished():
+	if pooping:
+		pooping=false
+		var poop=SHIT.instance()
+		poop.position=poopspawn
+		get_parent().add_child(poop)
+	pass # Replace with function body.
