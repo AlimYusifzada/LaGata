ca8a37a708f99c2eb21cf9e5ef887e024bc6856b
Alim Yusif-zada <yusifzaj@gmail.com>
2023-01-07 14:45:57 +0400

    jumping platforms update

--- Common/JumpingPlatforms/SpringJump.gd
+++ Common/JumpingPlatforms/SpringJump.gd
@@ -3,10 +3,14 @@ onready var AnSprite:AnimatedSprite=$AnSprite
 onready var APlayer:AnimationPlayer=$APlayer
 var jump:bool=false
 var jumper:Node=null
+export var JumPower=30
 func _ready():
 	pass # Replace with function body.
 func _on_Area2D_body_entered(body):
 	APlayer.play("bounce")
 	if body.is_in_group("Cats"):
-		body.emit_signal("Jump",30)
+		if body.velocity.y>0:
+			body.emit_signal("Jump",body.velocity.y/10+JumPower)
+		else:
+			body.emit_signal("Jump",JumPower)
 	pass # Replace with function body.
