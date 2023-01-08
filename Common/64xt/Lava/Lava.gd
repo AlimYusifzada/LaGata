29475f3675a9cedf4bffd1f9ff844e6002054abe
Alim Yusif-zada <yusifzaj@gmail.com>
2023-01-08 10:07:55 +0400

    sprites improve

--- Common/64xt/Lava/Lava.gd
+++ Common/64xt/Lava/Lava.gd
@@ -1,16 +1,16 @@
 extends Area2D
 
+onready var Smoke=preload("res://Common/JumpDust.tscn")
 
 func _ready():
-	
+	$Lava.play("default")
 	pass
 
 func _on_Lava_body_entered(body):
 	body.Life=false
-	$Smoke.visible=true
-	$Smoke.play("default")
+	var sm=Smoke.instance()
+	sm.position=body.position
+	sm.set_amount(10)
+	get_parent().add_child(sm)
 	pass # Replace with function body.
 
-func _on_Smoke_animation_finished():
-	$Smoke.visible=false
-	pass # Replace with function body.
--- Common/JumpDust.gd
+++ Common/JumpDust.gd
@@ -5,7 +5,6 @@ func _ready():
 	$EndTimer.start(1)
 	pass
 
-
 func _on_EndTimer_timeout():
 	queue_free()
 	pass # Replace with function body.
