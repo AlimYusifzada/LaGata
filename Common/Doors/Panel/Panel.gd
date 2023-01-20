4cafcdd8a3bfe88df1d4bb566b7a3cfb8f5985e5
Alim <yusifzaj@gmail.com>
2023-01-20 18:55:54 +0400

    sprites update

--- Common/Doors/Panel/Panel.gd
+++ Common/Doors/Panel/Panel.gd
@@ -1,25 +1,24 @@
 extends AnimatedSprite
 
 export var DoorName="DoorName"
-var Door:Node=null
+onready var Door=get_parent().get_node(DoorName)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	play("released")
-	Door=get_parent().get_node(DoorName)
 	if Door==null:
 		print("the door is not defined")
 		queue_free()
 	pass # Replace with function body.
 
 func _on_Area2D_body_entered(body):
-	if body.is_in_group("Movable"):
+	if body.is_in_group("Movable") && Door:
 		Door.emit_signal("SetOpen")
 		play("pressed")
 	pass # Replace with function body.
 
 func _on_Area2D_body_exited(body):
-	if body.is_in_group("Movable"):
+	if body.is_in_group("Movable") && Door:
 		Door.emit_signal("SetClose")
 		play("released")
 	pass # Replace with function body.
---
+++ Common/MovableObjects/MovableBlock.gd
@@ -0,0 +1,14 @@
+extends RigidBody2D
+
+var Broken=false
+
+func _ready():
+	pass
+
+func _process(delta):
+	if linear_velocity.y>500 && !Broken:
+		Broken=true
+	if Broken && is_equal_approx(linear_velocity.y,0.0):
+		$Label.text="broken"
+		set_mode(MODE_STATIC)
+		
